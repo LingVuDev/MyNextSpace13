97328c23023168cf67627cc104786128b310ca1f
Ling Vu <m.vu@thalia.de>
2023-07-23 13:31:59 +0200

    Add authentication

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface Post {
+  title: string;
+  content: string;
+  slug: string;
+}
+
+interface Props {
+  params: { slug: string };
+}
+
+
+const BlogPostPage = async ({ params }: Props) => {
+  const posts: Post[] = await fetch ('http://localhost:3000/api/content').then((res) => res.json());
+  const post = posts.find((post) => post.slug === params.slug)!;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </div>
+  )
+}
+
+export default BlogPostPage
