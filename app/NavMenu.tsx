25b27a909a1a31d61e235f5b4600ac862dd461dd
Ling Vu <m.vu@thalia.de>
2023-07-23 12:21:53 +0200

    Add static page

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import styles from './NavMenu.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const NavMenu = () => {
   return (
     <nav className={styles.nav}>
-      <Image
-        src="/logo.svg"
-        width={216}
-        height={30}
-        alt='MyNextSpace Logo'
-      />
+      <Link href={'/'}>
+        <Image
+          src="/logo.svg"
+          width={216}
+          height={30}
+          alt='MyNextSpace Logo'
+        />
+      </Link>
+      <ul className={styles.links}>
+        <li>
+          <Link href={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link href={'/blog'}>Blog</Link>
+        </li>
+        <li>
+          <Link href={'/users'}>Users</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
